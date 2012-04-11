366417da91fa9ce7fa01e590085cd039f4a638f9
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2012-04-11 21:50:52 +0000

    wrap open3 require so it doesn't blow up non-windows rubies

--- libraries/ruby_19_patches.rb
+++ libraries/ruby_19_patches.rb
@@ -1,6 +1,10 @@
 # patch to fix CHEF-2684
 if RUBY_VERSION =~ /^1\.8/
-  require 'win32/open3'
+  begin
+    require 'win32/open3'
+  rescue LoadError
+    require 'open3'
+  end
 else
   require 'open3'
 end
