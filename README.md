e9491777d3e2beae84c7b4ef880decc49f37f36b
Gert Kremer <gkremer@schubergphilis.com>
2013-03-26 11:44:47 +0100

    [COOK-2666] add starttime option to windows_task resource

--- README.md
+++ README.md
@@ -478,6 +478,7 @@ Server 2008 due to API usage.
 - run_level: Run with limited or highest privileges.
 - frequency: Frequency with which to run the task. (hourly, daily, ect.)
 - frequency_modifier: Multiple for frequency. (15 minutes, 2 days)
+- starttime: Optional start time for the scheduled task (HH:MM)
 
 ### Examples
 
--- resources/task.rb
+++ resources/task.rb
@@ -29,6 +29,7 @@ attribute :user, :kind_of => String, :default => nil
 attribute :password, :kind_of => String, :default => nil
 attribute :run_level, :equal_to => [:highest, :limited], :default => :limited
 attribute :frequency_modifier, :kind_of => Integer, :default => 1
+attribute :starttime, :kind_of => String, :default => nil, :regex => /[0-9][0-9]:[0-9][0-9]/
 attribute :frequency, :equal_to => [:minute,
                                     :hourly,
                                     :daily,
