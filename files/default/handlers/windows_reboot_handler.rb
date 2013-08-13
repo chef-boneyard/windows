f3238b502fa31d405c5a47e670294bfc390429c4
Joe Fitzgerald <joeyfitz@gmail.com>
2013-08-13 17:42:47 -0600

    Allowing default reboot timeout to be overridden
    - Useful in scenarios where a reboot is detected (vs. triggered via notification)

--- files/default/handlers/windows_reboot_handler.rb
+++ files/default/handlers/windows_reboot_handler.rb
@@ -19,7 +19,7 @@
 class WindowsRebootHandler < Chef::Handler
   include Chef::Mixin::ShellOut
 
-  def initialize(allow_pending_reboots = true, timeout = 60, reason = "Opscode Chef initiated reboot")
+  def initialize(allow_pending_reboots = true, timeout = node['windows']['reboot_timeout'], reason = "Opscode Chef initiated reboot")
     @allow_pending_reboots = allow_pending_reboots
     @timeout = timeout
     @reason = reason
