fe9482068635b99d6762320c8638d154cd9c9c67
Topher Cullen <topher@shawlite.com>
2013-09-23 18:56:54 -0400

    [COOK-3458] Add start_date and start_time in windows_task
    Signed-off-by: Seth Vargo <sethvargo@gmail.com>

--- resources/task.rb
+++ resources/task.rb
@@ -39,6 +39,8 @@ attribute :frequency, :equal_to => [:minute,
                                     :on_logon,
                                     :onstart,
                                     :on_idle], :default => :hourly
+attribute :start_day, :kind_of => String, :default => nil
+attribute :start_time, :kind_of => String, :default => nil
 
 attr_accessor :exists, :status
 
