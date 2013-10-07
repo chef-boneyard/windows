cd64abbb40596c8bca4656c7db0533da79a1a9d3
Greg Zapp <greg.zapp@gmail.com>
2013-10-08 10:09:50 +1300

    Removed extranous comments.

--- providers/feature_powershell.rb
+++ providers/feature_powershell.rb
@@ -9,15 +9,11 @@ include Chef::Mixin::PowershellOut
 include Windows::Helper
 
 def install_feature(name)
-  # return code 3010 is valid, it indicates a reboot is required
-  #shell_out!("#{dism} /online /enable-feature /featurename:#{@new_resource.feature_name} /norestart", {:returns => [0,42,127,3010]})
   cmd = powershell_out("Install-WindowsFeature #{@new_resource.feature_name}")
   Chef::Log.info(cmd.stdout)
 end
 
 def remove_feature(name)
-  # return code 3010 is valid, it indicates a reboot is required
-  #shell_out!("#{dism} /online /disable-feature /featurename:#{@new_resource.feature_name} /norestart", {:returns => [0,42,127,3010]})
   cmd = powershell_out("Uninstall-WindowsFeature #{@new_resource.feature_name}")
   Chef::Log.info(cmd.stdout)
 end
@@ -29,8 +25,6 @@ end
 
 def installed?
   @installed ||= begin
-    #cmd = shell_out("#{dism} /online /Get-Features", {:returns => [0,42,127]})
-    #cmd.stderr.empty? && (cmd.stdout =~  /^Feature Name : #{@new_resource.feature_name}.?$\n^State : Enabled.?$/i)
     cmd = powershell_out("Get-WindowsFeature #{@new_resource.feature_name} | Select Installed | % { Write-Host $_.Installed }")
     cmd.stderr.empty? &&  cmd.stdout =~ /True/i
   end
