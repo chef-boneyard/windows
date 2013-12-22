133b07b3894e0f5083e3af05746a6de9908b9c69
Christopher Armstrong <chris@chrisarmstrong.me>
2013-12-21 21:58:54 -0500

    Fixes typo

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.12.3'
 supports         'windows'
 depends          'chef_handler'
-depeneds         'powershell'
+depends          'powershell'
