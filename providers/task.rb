c85e1346fb73a5969386a20efdfedcc29577d165
Greg Zapp <greg.zapp@gmail.com>
2014-02-14 16:19:06 -0500

    [COOK-3715] - Unable to create a startup task with no login
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- providers/task.rb
+++ providers/task.rb
@@ -25,6 +25,7 @@ action :create do
   if @current_resource.exists
     Chef::Log.info "#{@new_resource} task already exists - nothing to do"
   else
+    if @new_resource.user and @new_resource.password.nil? then Chef::Log.debug "#{@new_resource} did not specify a password, creating task without a password" end
     use_force = @new_resource.force ? '/F' : ''
     cmd =  "schtasks /Create #{use_force} /TN \"#{@new_resource.name}\" "
     schedule  = @new_resource.frequency == :on_logon ? "ONLOGON" : @new_resource.frequency
@@ -33,11 +34,8 @@ action :create do
     cmd += "/SD \"#{@new_resource.start_day}\" " unless @new_resource.start_day.nil?
     cmd += "/ST \"#{@new_resource.start_time}\" " unless @new_resource.start_time.nil?
     cmd += "/TR \"#{@new_resource.command}\" "
-    if @new_resource.user && @new_resource.password
-      cmd += "/RU \"#{@new_resource.user}\" /RP \"#{@new_resource.password}\" "
-    elsif (@new_resource.user and !@new_resource.password) || (@new_resource.password and !@new_resource.user)
-      Chef::Log.fatal "#{@new_resource.name}: Can't specify user or password without both!"
-    end
+    cmd += "/RU \"#{@new_resource.user}\" " if @new_resource.user
+    cmd += "/RP \"#{@new_resource.password}\" " if @new_resource.user and @new_resource.password
     cmd += "/RL HIGHEST " if @new_resource.run_level == :highest
     shell_out!(cmd, {:returns => [0]})
     @new_resource.updated_by_last_action true
