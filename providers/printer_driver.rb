c184318ee01f8d6519f19a10dbee2c9e06248335
Derek Groh <dgroh@arch.tamu.edu>
2014-03-17 12:39:08 -0500

    use apache 2.0 license, standardize spacing, removed duplication in #driver_exists

--- providers/printer_driver.rb
+++ providers/printer_driver.rb
@@ -5,44 +5,38 @@
 #
 # Copyright 2013, Texas A&M
 #
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 #
 require 'mixlib/shellout'
 
-action :create do
+action :install do
   if driver_exists?
     Chef::Log.info("#{ new_resource.name } already exists - nothing to do.")
     new_resource.updated_by_last_action(false)
   else
     windows_batch "Creating print driver: #{ new_resource.name }" do
-      code "rundll32 printui.dll PrintUIEntry /ia /m \"#{new_resource.name }\" /h \"#{ new_resource.environment}\" /v \"#{new_resource.version }\" /f \"#{new_resource.inf_path}\""
+      code "rundll32 printui.dll PrintUIEntry /ia /m \"#{ new_resource.name }\" /h \"#{ new_resource.environment}\" /v \"#{ new_resource.version }\" /f \"#{ new_resource.inf_path}\""
     end
     Chef::Log.info("#{ new_resource.name } installed.")
     new_resource.updated_by_last_action(true)
   end
 end
 
-action :delete do
+action :remove do
   if driver_exists?
     windows_batch "Deleting print driver: #{ new_resource.name }" do
-      code "rundll32 printui.dll PrintUIEntry /dd /m \"#{new_resource.name}\" /h \"#{new_resource.environment}\" /v \"#{new_resource.version}\""
+      code "rundll32 printui.dll PrintUIEntry /dd /m \"#{ new_resource.name}\" /h \"#{new_resource.environment }\" /v \"#{ new_resource.version}\""
     end
     Chef::Log.info("#{ new_resource.name } uninstalled.")
     new_resource.updated_by_last_action(true)
@@ -53,15 +47,17 @@ action :delete do
 end
   
 def driver_exists?
+  check = "Mixlib::ShellOut.new(\"powershell.exe \"Get-wmiobject -Class Win32_PrinterDriver -EnableAllPrivileges | where {$_.name -like '#{ new_resource.name},3,"
   case new_resource.environment
   when "x64"
-    check = Mixlib::ShellOut.new("powershell.exe \"Get-wmiobject -Class Win32_PrinterDriver -EnableAllPrivileges | where {$_.name -like '#{new_resource.name},3,Windows x64'} | fl name\"").run_command
+    check << "Windows x64"
   when "x86"
-    check = Mixlib::ShellOut.new("powershell.exe \"Get-wmiobject -Class Win32_PrinterDriver -EnableAllPrivileges | where {$_.name -like '#{new_resource.name},3,Windows NT x86'} | fl name\"").run_command
+    check << "Windows NT x86"
   when "Itanium"
-    check = Mixlib::ShellOut.new("powershell.exe \"Get-wmiobject -Class Win32_PrinterDriver -EnableAllPrivileges | where {$_.name -like '#{new_resource.name},3,Itanium'} | fl name\"").run_command
+    check << "Itanium"
   else
     Chef::Log.error("Please use \"x64\", \"x86\" or \"Itanium\" as the environment type")
   end
-  check.stdout.include? "#{new_resource.name}"
+  check << "'} | fl name\"\").run_command"
+  check.stdout.include? "#{ new_resource.name }"
 end
