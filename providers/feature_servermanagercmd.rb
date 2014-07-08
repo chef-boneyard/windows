204fb1c58ef1b43f72e90fbc5058b8d57984059f
Topher Cullen <topher@shawlite.com>
2014-07-08 10:36:09 +0100

    Shift servermanagercmd_returns into a proper private variable...

--- providers/feature_servermanagercmd.rb
+++ providers/feature_servermanagercmd.rb
@@ -22,25 +22,28 @@ include Chef::Provider::WindowsFeature::Base
 include Chef::Mixin::ShellOut
 include Windows::Helper
 
+private
+# From http://technet.microsoft.com/en-us/library/cc749128(v=ws.10).aspx
+@servermanagercmd_returns = [0,42,127,1003]
+
+public
+
 def install_feature(name)
-  shell_out!("#{servermanagercmd} -install #{@new_resource.feature_name}", {:returns => servermanagercmd_returns})
+  shell_out!("#{servermanagercmd} -install #{@new_resource.feature_name}", {:returns => @servermanagercmd_returns})
 end
 
 def remove_feature(name)
-  shell_out!("#{servermanagercmd} -remove #{@new_resource.feature_name}", {:returns => servermanagercmd_returns})
+  shell_out!("#{servermanagercmd} -remove #{@new_resource.feature_name}", {:returns => @servermanagercmd_returns})
 end
 
 def installed?
   @installed ||= begin
-    cmd = shell_out("#{servermanagercmd} -query", {:returns => servermanagercmd_returns})
+    cmd = shell_out("#{servermanagercmd} -query", {:returns => @servermanagercmd_returns})
     cmd.stderr.empty? && (cmd.stdout =~ /^\s*?\[X\]\s.+?\s\[#{@new_resource.feature_name}\]\s*$/i)
   end
 end
 
 private
-
-# From http://technet.microsoft.com/en-us/library/cc749128(v=ws.10).aspx
-servermanagercmd_returns = [0,42,127,1003]
 
 # account for File System Redirector
 # http://msdn.microsoft.com/en-us/library/aa384187(v=vs.85).aspx
