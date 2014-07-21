2f35b3be60cb17b0e8400059c405dcc55622faaf
Venkat Naidu <Venkat.Naidu@e-hps.com>
2014-07-21 14:36:38 -0400

    #87 - support simple windows folder sharing
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- recipes/reboot_handler.rb
+++ recipes/reboot_handler.rb
@@ -27,6 +27,6 @@ end
 chef_handler 'WindowsRebootHandler' do
   source "#{node['chef_handler']['handler_path']}/windows_reboot_handler.rb"
   arguments node['windows']['allow_pending_reboots']
-  supports :report => true, :exception => false
+  supports :report => true, :exception => node['windows']['allow_reboot_on_failure']
   action :enable
 end
