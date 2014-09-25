aa1d8b3192c70422440c0ca4190971be57af41f8
Baptiste Courtois <b.courtois@criteo.com>
2014-09-25 17:38:20 +0200

    Move reboot_pending? method to windows_helper
    Move method reboot_pending? from reboot_handler to windows_helper.
    This allows thirdparty providers to use the detection code.

--- libraries/windows_helper.rb
+++ libraries/windows_helper.rb
@@ -96,6 +96,19 @@ module Windows
       buf.strip
     end
 
+    # traditional ways to determine if windows has pending reboot
+    def self.reboot_pending?
+      # Any files listed here means reboot needed
+      (::Registry.key_exists?('HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\PendingFileRenameOperations') &&
+        ::Registry.get_value('HKLM\SYSTEM\CurrentControlSet\Control\Session Manager', 'PendingFileRenameOperations').any?) ||
+      # 1 for any value means reboot pending
+      # "9306cdfc-c4a1-4a22-9996-848cb67eddc3"=1
+      (::Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired') &&
+        ::Registry.get_values('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired').select { |v| v[2] == 1 }.any?) ||
+      # 1 or 2 for 'Flags' value means reboot pending
+      (::Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Updates\UpdateExeVolatile') &&
+        [1, 2].include?(::Registry::get_value('HKLM\SOFTWARE\Microsoft\Updates\UpdateExeVolatile', 'Flags')))
+    end
   end
 end
 
