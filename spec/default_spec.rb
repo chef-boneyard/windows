c57bd90d65435a850ff220bf8eba22fe777454d2
jbrody-stewart <Jason.Brody-Stewart>
2015-01-10 16:53:01 -0800

    Adding both windows 2008R2 and 2012R2

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,17 +1,35 @@
 require 'spec_helper'
 
 describe 'windows::default' do
-  let(:chef_run) do
-    runner = ChefSpec::Runner.new(platform: 'windows', version: '2012R2')
-    runner.converge(described_recipe)
+  context '2012R2' do
+    version = '2012R2'
+    let(:chef_run) do
+      runner = ChefSpec::Runner.new(platform: 'windows', version: version)
+      runner.converge(described_recipe)
+    end
+
+    gems=%w(win32-api win32-service windows-api windows-pr win32-dir win32-event win32-mutex)
+    gems.each() do |gem|
+      it "should install gems #{gems}" do
+        expect(chef_run).to install_chef_gem(gem)
+      end
+    end
   end
 
-  gems=%w(win32-api win32-service windows-api windows-pr win32-dir win32-event win32-mutex)
-  gems.each() do |gem|
-  it 'should include the community windows recipe' do
-    expect(chef_run).to install_chef_gem(gem)
-  end
+  context '2008R2' do
+    version = '2008R2'
+    let(:chef_run) do
+      runner = ChefSpec::Runner.new(platform: 'windows', version: version)
+      runner.converge(described_recipe)
     end
+
+    gems=%w(win32-api win32-service windows-api windows-pr win32-dir win32-event win32-mutex)
+    gems.each() do |gem|
+      it "should install gems #{gems}" do
+        expect(chef_run).to install_chef_gem(gem)
+      end
+    end
+  end
 
 end
 
