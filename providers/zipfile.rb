9683c4a12868ab4994b1ba525e683883702cb3f5
Topher Cullen <topher@shawlite.com>
2015-02-19 00:47:55 -0500

    fixing zipfile provider

--- providers/zipfile.rb
+++ providers/zipfile.rb
@@ -85,7 +85,7 @@ def ensure_rubyzip_gem_installed
   rescue LoadError
     Chef::Log.info("Missing gem 'rubyzip'...installing now.")
     chef_gem "rubyzip" do
-      compile_time true if respond_to?(:compile_time)
+      compile_time true if Chef::Resource::ChefGem.method_defined?(:compile_time)
       version node['windows']['rubyzipversion']
       action :install
     end
