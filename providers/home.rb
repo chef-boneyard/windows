18a250547f553a660744468eaf09fd956cc3f260
Dennis Hoer <dennis.hoer@pearson.com>
2015-03-09 21:06:01 -0500

    Fix home security issues

--- providers/home.rb
+++ providers/home.rb
@@ -2,26 +2,38 @@ require 'chef/mixin/shell_out'
 include Chef::Mixin::ShellOut
 
 action :create do
-  task_name = "create_#{@new_resource.name}_home"
-  user_home = ::File.join(ENV['SYSTEMDRIVE'], 'Users', @new_resource.name)
-    .gsub(::File::SEPARATOR, ::File::ALT_SEPARATOR)
+  task_name = "build_#{new_resource.name}_home"
+  user_home = ::File.join(ENV['SYSTEMDRIVE'], 'Users', new_resource.name)
+              .gsub(::File::SEPARATOR, ::File::ALT_SEPARATOR)
 
-  # Run whoami as scheduled task to get LocalServer group privilege:
+  # Create whoami as scheduled task to get Local Server group privilege:
   # SeAssignPrimaryTokenPrivilege (Replace a process-level token)
-  if task_query(task_name).empty?
-    cmd =  "schtasks /Create /TN \"#{task_name}\" "
-    cmd += "/SC once /SD \"01/01/2003\" /ST \"00:00\" /TR \"whoami.exe\" "
-    cmd += "/RU \"#{@new_resource.name}\" /RP \"#{@new_resource.password}\" "
-    cmd += "/RL HIGHEST "
-    shell_out!(cmd, {:returns => [0]})
+  execute "create_#{task_name}_task" do
+    command <<-EOF
+schtasks /Create /TN "#{task_name}" /SC once /SD "01/01/2003" /ST "00:00" \
+/TR "whoami.exe" /RU "#{new_resource.name}" /RP "#{new_resource.password}" \
+/RL HIGHEST
+EOF
+    sensitive true
+    only_if { task_query(task_name).empty? }
   end
 
-  unless ::File.exist?(user_home)
-    shell_out!("schtasks /Run /TN \"#{task_name}\"", {:returns => [0]})
+  execute "run_#{task_name}_task" do
+    command "schtasks /Run /TN \"#{task_name}\""
+    not_if { ::File.exist?(user_home) }
   end
 
-  # wait until task completed
-  loop { break unless task_status(task_name) == 'Running' }
+  ruby_block "wait_until_#{task_name}_task_completed" do
+    block do
+      sleep(1) until task_completed?(task_name)
+    end
+    action :run
+  end
+
+  execute "delete_#{task_name}_task" do
+    command "schtasks /Delete /TN \"#{task_name}\" /F"
+    not_if { task_query(task_name).empty? }
+  end
 
   new_resource.updated_by_last_action true
   Chef::Log.info "#{user_home} created"
@@ -31,16 +43,8 @@ def task_query(task_name)
   shell_out("schtasks /Query /FO LIST /V /TN \"#{task_name}\"").stdout
 end
 
-def task_status(task_name)
-  task = Hash.new
-  task_query(task_name).split("\n").map! do |line|
-    line.split(":", 2).map! do |field|
-      field.strip
-    end
-  end.each do |field|
-    if field.kind_of? Array and field[0].respond_to? :to_sym
-      task[field[0].gsub(/\s+/,"").to_sym] = field[1]
-    end
-  end
-  task[:Status]
+def task_completed?(task_name)
+  out = task_query(task_name)
+  status = /^Status:(.*)$/.match(out).captures[0]
+  status.strip == 'Ready'
 end
