b53b1c4e93a441a251053f08b60f88d65e04b95e
Topher Cullen <topher@shawlite.com>
2015-05-14 22:07:49 +0000

    a few more tests

--- test/cookbooks/minimal/recipes/path.rb
+++ test/cookbooks/minimal/recipes/path.rb
@@ -9,3 +9,9 @@ directory 'c:\path_test_another_path'
 windows_path 'C:\path_test_another_path' do
   action :add
 end
+
+powershell_script 'Log Path' do
+  code <<-EOH
+    $env:path -split ';' | out-file c:\\paths.txt
+  EOH
+end
