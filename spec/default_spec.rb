850aecf73bf8404558e9cdc6551f7d7f27ec6184
Joel Handwell <joelhandwell@gmail.com>
2015-05-18 13:44:48 -0400

    Add chefspec test for default recipe

---
+++ spec/default_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'spec_helper'
+
+describe 'windows::default' do
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+  %w{ win32-api win32-service }.each do |win_gem|
+    it "installes precompiled binary of #{win_gem} gem" do
+      expect(chef_run).to install_chef_gem(win_gem).with(options: '--platform=mswin32')
+    end
+  end
+
+  %w{ windows-api windows-pr win32-dir win32-event win32-mutex }.each do |win_gem|
+    it "installes #{win_gem} gem" do
+      expect(chef_run).to install_chef_gem(win_gem)
+    end
+  end
+end
