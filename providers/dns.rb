67b143e6fa0169c816b592067e685479ea02ea9a
Richard Lavey <richard.lavey@calastone.com>
2015-05-22 08:43:52 +0100

    replace \t with 2 spaces

--- providers/dns.rb
+++ providers/dns.rb
@@ -30,125 +30,125 @@ def whyrun_supported?
 end
 
 action :create do
-	if @current_resource.exists
-		needsChange = (@new_resource.record_type != @current_resource.record_type) ||
-			(@new_resource.ttl > 0 && @new_resource.ttl != @current_resource.ttl) ||
-			(@new_resource.target.is_a?(String) && @new_resource.target != @current_resource.target) ||
-			(@new_resource.target.is_a?(Array) && !(@new_resource.target - @current_resource.target).empty?)
+  if @current_resource.exists
+    needsChange = (@new_resource.record_type != @current_resource.record_type) ||
+      (@new_resource.ttl > 0 && @new_resource.ttl != @current_resource.ttl) ||
+      (@new_resource.target.is_a?(String) && @new_resource.target != @current_resource.target) ||
+      (@new_resource.target.is_a?(Array) && !(@new_resource.target - @current_resource.target).empty?)
 
-		if needsChange
-			converge_by("Changing #{ @new_resource.host_name }") do
-				updateDns
-			end
-		else
-			Chef::Log.debug("#{@new_resource.host_name} already exists - nothing to do")
-		end
-	else
-		converge_by("Creating #{ @new_resource.host_name }") do
-			updateDns
-		end
-	end
+    if needsChange
+      converge_by("Changing #{ @new_resource.host_name }") do
+        updateDns
+      end
+    else
+      Chef::Log.debug("#{@new_resource.host_name} already exists - nothing to do")
+    end
+  else
+    converge_by("Creating #{ @new_resource.host_name }") do
+      updateDns
+    end
+  end
 
 end
 
 action :delete do
-	if @current_resource.exists 
-		converge_by("Deleting #{ @current_resource.host_name }") do
-			executeCommand! 'recorddelete', "#{@current_resource.record_type} /f"
-		end
-	else
-		Chef::Log.debug("#{@new_resource.host_name} does not exist - nothing to do")
-	end
+  if @current_resource.exists 
+    converge_by("Deleting #{ @current_resource.host_name }") do
+      executeCommand! 'recorddelete', "#{@current_resource.record_type} /f"
+    end
+  else
+    Chef::Log.debug("#{@new_resource.host_name} does not exist - nothing to do")
+  end
 end
 
 def load_current_resource
-	# validate the new resource params : A records should be an array
-	if @new_resource.record_type == 'A' && @new_resource.target.is_a?(String)
-		raise "target property must be an array for record_type A"
-	end
-	
-	@current_resource = Chef::Resource::WindowsDns.new(@new_resource.name)
-	@current_resource.host_name(@new_resource.host_name)
-	@current_resource.dns_server(@new_resource.dns_server)
-	
-	parts = @current_resource.host_name.scan(/(\w+)\.(.*)/)
-	@host = parts[0][0]
-	@domain = parts[0][1]
+  # validate the new resource params : A records should be an array
+  if @new_resource.record_type == 'A' && @new_resource.target.is_a?(String)
+    raise "target property must be an array for record_type A"
+  end
+  
+  @current_resource = Chef::Resource::WindowsDns.new(@new_resource.name)
+  @current_resource.host_name(@new_resource.host_name)
+  @current_resource.dns_server(@new_resource.dns_server)
+  
+  parts = @current_resource.host_name.scan(/(\w+)\.(.*)/)
+  @host = parts[0][0]
+  @domain = parts[0][1]
 
-	getAttributes
+  getAttributes
 end
 
 private
 def getAttributes
-	@command = locate_sysnative_cmd("dnscmd.exe")
-	cmd = shell_out("#{@command} #{@current_resource.dns_server} /enumrecords #{@domain} #{@host}")
-	Chef::Log.debug "dnscmd reports: #{cmd.stdout}"
-	
-	# extract values from returned text
-	if cmd.stdout.include?('DNS_ERROR_NAME_DOES_NOT_EXIST')
-		@current_resource.exists = false
-		@current_resource.target([])
-	elsif cmd.exitstatus == 0
-		@current_resource.exists = true
-		
-		m = cmd.stdout.scan(/(\d+)\s(A)\s+(\d+\.\d+\.\d+\.\d+)/)
-		if m.length == 0
-			m = cmd.stdout.scan(/(\d+)\s(CNAME)\s+((?:\w+\.)+)/)
-			if m.length == 0
-				@current_resource.exists = false		
-				@current_resource.target([])
-			else
-				# We have a cname record
-				@current_resource.record_type('CNAME')
-				@current_resource.ttl(m[0][0].to_i)
-				@current_resource.target(m[0][2].chomp('.'))
-			end
-		else
-			# we have A entries
-			@current_resource.record_type('A')
-			@current_resource.ttl(m[0][0].to_i)
-			addresses = []
-			m.each do | match |
-				addresses.push(match[2])
-			end
-			@current_resource.target(addresses)
-		end
-	else
-		raise "dnscmd returned error #{cmd.exitstatus} : #{cmd.stderr} #{cmd.stdout}"
-	end
+  @command = locate_sysnative_cmd("dnscmd.exe")
+  cmd = shell_out("#{@command} #{@current_resource.dns_server} /enumrecords #{@domain} #{@host}")
+  Chef::Log.debug "dnscmd reports: #{cmd.stdout}"
+  
+  # extract values from returned text
+  if cmd.stdout.include?('DNS_ERROR_NAME_DOES_NOT_EXIST')
+    @current_resource.exists = false
+    @current_resource.target([])
+  elsif cmd.exitstatus == 0
+    @current_resource.exists = true
+    
+    m = cmd.stdout.scan(/(\d+)\s(A)\s+(\d+\.\d+\.\d+\.\d+)/)
+    if m.length == 0
+      m = cmd.stdout.scan(/(\d+)\s(CNAME)\s+((?:\w+\.)+)/)
+      if m.length == 0
+        @current_resource.exists = false    
+        @current_resource.target([])
+      else
+        # We have a cname record
+        @current_resource.record_type('CNAME')
+        @current_resource.ttl(m[0][0].to_i)
+        @current_resource.target(m[0][2].chomp('.'))
+      end
+    else
+      # we have A entries
+      @current_resource.record_type('A')
+      @current_resource.ttl(m[0][0].to_i)
+      addresses = []
+      m.each do | match |
+        addresses.push(match[2])
+      end
+      @current_resource.target(addresses)
+    end
+  else
+    raise "dnscmd returned error #{cmd.exitstatus} : #{cmd.stderr} #{cmd.stdout}"
+  end
 end
 
 def updateDns
-	ttl = @new_resource.ttl if @new_resource.ttl > 0
+  ttl = @new_resource.ttl if @new_resource.ttl > 0
 
-	if (@current_resource.record_type != @new_resource.record_type)
-		# delete current record(s) as we're changing the type
-		executeCommand! 'recorddelete', "#{@current_resource.record_type} /f"
-	end
-	
-	if @new_resource.record_type == 'A'
-		# delete existing records that are no longer defined
-		(@current_resource.target - @new_resource.target).each do | address |
-			Chef::Log.info "Deleting #{address}"
-			executeCommand! 'recorddelete', "A #{address} /f"
-		end
-		
-		# add new records that don't exist
-		# if ttl has changed then update all records
-		if (@current_resource.ttl == @new_resource.ttl)
-			addresses = (@new_resource.target - @current_resource.target)
-		else
-			addresses = @new_resource.target
-		end
-		addresses.each do | address |
-			Chef::Log.info "Adding/Changing #{address}"
-			executeCommand! 'recordadd', "#{ttl} A #{address}"
-		end
-	else
-		executeCommand! 'recordadd', "#{ttl} CNAME #{@new_resource.target}"
-	end
+  if (@current_resource.record_type != @new_resource.record_type)
+    # delete current record(s) as we're changing the type
+    executeCommand! 'recorddelete', "#{@current_resource.record_type} /f"
+  end
+  
+  if @new_resource.record_type == 'A'
+    # delete existing records that are no longer defined
+    (@current_resource.target - @new_resource.target).each do | address |
+      Chef::Log.info "Deleting #{address}"
+      executeCommand! 'recorddelete', "A #{address} /f"
+    end
+    
+    # add new records that don't exist
+    # if ttl has changed then update all records
+    if (@current_resource.ttl == @new_resource.ttl)
+      addresses = (@new_resource.target - @current_resource.target)
+    else
+      addresses = @new_resource.target
+    end
+    addresses.each do | address |
+      Chef::Log.info "Adding/Changing #{address}"
+      executeCommand! 'recordadd', "#{ttl} A #{address}"
+    end
+  else
+    executeCommand! 'recordadd', "#{ttl} CNAME #{@new_resource.target}"
+  end
 end
 
 def executeCommand! (mode, options)
-	shell_out!("#{@command} #{@current_resource.dns_server} /#{mode} #{@domain} #{@host} #{options}")
+  shell_out!("#{@command} #{@current_resource.dns_server} /#{mode} #{@domain} #{@host} #{options}")
 end
