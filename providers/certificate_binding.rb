68ce3e8ebd90c69371b842a44aa5d33be31b5a8e
Topher Cullen <topher@shawlite.com>
2015-05-22 08:49:17 +0100

    replaced \t with 2 spaces

--- providers/certificate_binding.rb
+++ providers/certificate_binding.rb
@@ -30,92 +30,92 @@ def whyrun_supported?
 end
 
 action :create do
-	hash = @new_resource.name_kind == :subject ? getHashFromSubject() : @new_resource.cert_name
+  hash = @new_resource.name_kind == :subject ? getHashFromSubject() : @new_resource.cert_name
 
-	if @current_resource.exists
-		needsChange = (hash.casecmp(@current_hash) != 0)
+  if @current_resource.exists
+    needsChange = (hash.casecmp(@current_hash) != 0)
 
-		if needsChange
-			converge_by("Changing #{@current_resource.address}:#{@current_resource.port}") do
-				deleteBinding
-				setBinding hash
-			end
-		else
-			Chef::Log.debug("#{@current_resource.address}:#{@current_resource.port} already bound to #{hash} - nothing to do")
-		end
-	else
-		converge_by("Binding #{@current_resource.address}:#{@current_resource.port}") do
-			setBinding hash
-		end
-	end
+    if needsChange
+      converge_by("Changing #{@current_resource.address}:#{@current_resource.port}") do
+        deleteBinding
+        setBinding hash
+      end
+    else
+      Chef::Log.debug("#{@current_resource.address}:#{@current_resource.port} already bound to #{hash} - nothing to do")
+    end
+  else
+    converge_by("Binding #{@current_resource.address}:#{@current_resource.port}") do
+      setBinding hash
+    end
+  end
 end
 
 action :delete do
-	if @current_resource.exists 
-		converge_by("Deleting #{@current_resource.address}:#{@current_resource.port}") do
-			deleteBinding
-		end
-	else
-		Chef::Log.debug("#{@current_resource.address}:#{@current_resource.port} not bound - nothing to do")
-	end
+  if @current_resource.exists 
+    converge_by("Deleting #{@current_resource.address}:#{@current_resource.port}") do
+      deleteBinding
+    end
+  else
+    Chef::Log.debug("#{@current_resource.address}:#{@current_resource.port} not bound - nothing to do")
+  end
 end
 
 def load_current_resource
-	@current_resource = Chef::Resource::WindowsCertificateBinding.new(@new_resource.name)
-	@current_resource.cert_name(@new_resource.cert_name)
-	@current_resource.name_kind(@new_resource.name_kind)
-	@current_resource.address(@new_resource.address)
-	@current_resource.port(@new_resource.port)
+  @current_resource = Chef::Resource::WindowsCertificateBinding.new(@new_resource.name)
+  @current_resource.cert_name(@new_resource.cert_name)
+  @current_resource.name_kind(@new_resource.name_kind)
+  @current_resource.address(@new_resource.address)
+  @current_resource.port(@new_resource.port)
 
-	@command = locate_sysnative_cmd("netsh.exe")
-	getCurrentHash
+  @command = locate_sysnative_cmd("netsh.exe")
+  getCurrentHash
 end
 
 private
 def getCurrentHash()
-	cmd = shell_out("#{@command} http show sslcert ipport=#{@current_resource.address}:#{@current_resource.port}")
-	Chef::Log.debug "netsh reports: #{cmd.stdout}"
+  cmd = shell_out("#{@command} http show sslcert ipport=#{@current_resource.address}:#{@current_resource.port}")
+  Chef::Log.debug "netsh reports: #{cmd.stdout}"
 
-	if cmd.exitstatus == 0
-		m = cmd.stdout.scan(/Certificate Hash\s+:\s?(\w{40})/)
-		if m.length == 0
-			raise "Failed to extract hash from command output #{cmd.stdout}"
-		else
-			@current_hash = m[0][0]
-			@current_resource.exists = true
-		end		
-	else
-		@current_resource.exists = false
-	end
+  if cmd.exitstatus == 0
+    m = cmd.stdout.scan(/Certificate Hash\s+:\s?(\w{40})/)
+    if m.length == 0
+      raise "Failed to extract hash from command output #{cmd.stdout}"
+    else
+      @current_hash = m[0][0]
+      @current_resource.exists = true
+    end    
+  else
+    @current_resource.exists = false
+  end
 end
 
 def setBinding(hash)
-	cmd = "#{@command} http add sslcert"
-	cmd << " ipport=#{@current_resource.address}:#{@current_resource.port}" 
-	cmd << " certhash=#{hash}"
-	cmd << " appid=#{@current_resource.app_id}"
-	cmd << " certstorename=#{@current_resource.store_name}"
-	
-	shell_out!(cmd)
+  cmd = "#{@command} http add sslcert"
+  cmd << " ipport=#{@current_resource.address}:#{@current_resource.port}" 
+  cmd << " certhash=#{hash}"
+  cmd << " appid=#{@current_resource.app_id}"
+  cmd << " certstorename=#{@current_resource.store_name}"
+  
+  shell_out!(cmd)
 end
 
 def deleteBinding()
-	shell_out!("#{@command} http delete sslcert ipport=#{@current_resource.address}:#{@current_resource.port}")
+  shell_out!("#{@command} http delete sslcert ipport=#{@current_resource.address}:#{@current_resource.port}")
 end
 
 def getHashFromSubject()
-	# escape wildcard subject name (*.acme.com)
-	subject = @current_resource.cert_name.sub(/\*/, '`*')
-	ps_script = "& { gci cert:\\localmachine\\#{@current_resource.store_name} | where subject -like '*#{subject}*' | select -first 1 -expandproperty Thumbprint }"
+  # escape wildcard subject name (*.acme.com)
+  subject = @current_resource.cert_name.sub(/\*/, '`*')
+  ps_script = "& { gci cert:\\localmachine\\#{@current_resource.store_name} | where subject -like '*#{subject}*' | select -first 1 -expandproperty Thumbprint }"
 
-	Chef::Log.debug "Running PS script #{ps_script}"
-	p = powershell_out!(ps_script)
-	
-	if (!p.stderr.nil? && p.stderr.length > 0)
-		raise "#{ps_script} failed with #{p.stderr}"
-	elsif (p.stdout.nil? || p.stdout.length == 0)
-		raise "Couldn't find thumbprint for subject #{@current_resource.cert_name}"
-	end
+  Chef::Log.debug "Running PS script #{ps_script}"
+  p = powershell_out!(ps_script)
+  
+  if (!p.stderr.nil? && p.stderr.length > 0)
+    raise "#{ps_script} failed with #{p.stderr}"
+  elsif (p.stdout.nil? || p.stdout.length == 0)
+    raise "Couldn't find thumbprint for subject #{@current_resource.cert_name}"
+  end
 
-	p.stdout.strip
+  p.stdout.strip
 end
