741354b5c5dd639f4c7095da77b6180d7d0f93cf
Topher Cullen <topher@shawlite.com>
2015-05-22 08:48:15 +0100

    replaced \t with 2 spaces

--- providers/http_acl.rb
+++ providers/http_acl.rb
@@ -29,62 +29,62 @@ def whyrun_supported?
 end
 
 action :create do
-	raise "No user property set" if @new_resource.user.nil? || @new_resource.user.empty?
-	
-	if @current_resource.exists
-		needsChange = (@current_resource.user.casecmp(@new_resource.user) != 0)
+  raise "No user property set" if @new_resource.user.nil? || @new_resource.user.empty?
+  
+  if @current_resource.exists
+    needsChange = (@current_resource.user.casecmp(@new_resource.user) != 0)
 
-		if needsChange
-			converge_by("Changing #{@current_resource.url}") do
-				deleteAcl
-				setAcl
-			end
-		else
-			Chef::Log.debug("#{@current_resource.url} already set - nothing to do")
-		end
-	else
-		converge_by("Setting #{@current_resource.url}") do
-			setAcl
-		end
-	end
+    if needsChange
+      converge_by("Changing #{@current_resource.url}") do
+        deleteAcl
+        setAcl
+      end
+    else
+      Chef::Log.debug("#{@current_resource.url} already set - nothing to do")
+    end
+  else
+    converge_by("Setting #{@current_resource.url}") do
+      setAcl
+    end
+  end
 end
 
 action :delete do
-	if @current_resource.exists 
-		converge_by("Deleting #{@current_resource.url}") do
-			deleteAcl
-		end
-	else
-		Chef::Log.debug("#{@current_resource.url} does not exist - nothing to do")
-	end
+  if @current_resource.exists 
+    converge_by("Deleting #{@current_resource.url}") do
+      deleteAcl
+    end
+  else
+    Chef::Log.debug("#{@current_resource.url} does not exist - nothing to do")
+  end
 end
 
 def load_current_resource
-	@current_resource = Chef::Resource::WindowsHttpAcl.new(@new_resource.name)
-	@current_resource.url(@new_resource.url)
+  @current_resource = Chef::Resource::WindowsHttpAcl.new(@new_resource.name)
+  @current_resource.url(@new_resource.url)
 
-	@command = locate_sysnative_cmd("netsh.exe")
-	getCurrentAcl
+  @command = locate_sysnative_cmd("netsh.exe")
+  getCurrentAcl
 end
 
 private
 def getCurrentAcl()
-	cmd = shell_out!("#{@command} http show urlacl url=#{@current_resource.url}")
-	Chef::Log.debug "netsh reports: #{cmd.stdout}"
+  cmd = shell_out!("#{@command} http show urlacl url=#{@current_resource.url}")
+  Chef::Log.debug "netsh reports: #{cmd.stdout}"
 
-	m = cmd.stdout.scan(/User:\s*(\S+)/)
-	if m.length == 0
-		@current_resource.exists = false
-	else
-		@current_resource.user(m[0][0])
-		@current_resource.exists = true
-	end		
+  m = cmd.stdout.scan(/User:\s*(\S+)/)
+  if m.length == 0
+    @current_resource.exists = false
+  else
+    @current_resource.user(m[0][0])
+    @current_resource.exists = true
+  end    
 end
 
 def setAcl()
-	shell_out!("#{@command} http add urlacl url=#{@new_resource.url} user=#{@new_resource.user}")
+  shell_out!("#{@command} http add urlacl url=#{@new_resource.url} user=#{@new_resource.user}")
 end
 
 def deleteAcl()
-	shell_out!("#{@command} http delete urlacl url=#{@new_resource.url}")
+  shell_out!("#{@command} http delete urlacl url=#{@new_resource.url}")
 end
