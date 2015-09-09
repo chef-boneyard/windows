cb04abf20567076a6793236c5b21d3013e050879
Tim Smith <tsmith84@gmail.com>
2015-09-09 10:32:11 -0700

    Update rubocop rules and resolve warnings in tests

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Provides a set of useful Windows-specific primitives.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.38.1'
 supports         'windows'
-source_url       "https://github.com/chef-cookbooks/windows" if respond_to?(:source_url)
-issues_url       "https://github.com/chef-cookbooks/windows/issues" if respond_to?(:issues_url)
+source_url       'https://github.com/chef-cookbooks/windows' if respond_to?(:source_url)
+issues_url       'https://github.com/chef-cookbooks/windows/issues' if respond_to?(:issues_url)
 depends          'chef_handler'
