chef-boneyard/windows

commit 7471d87e6ca4e79eeffa67cb540669d912071852

Reboot handler use chef DSL with chef > 11.12

files/default/handlers/windows_reboot_handler.rb
@@ -51,21 +51,25 @@ class WindowsRebootHandler < Chef::Handler
     node.run_state[:reboot_requested] == true
   end
 
-  # reboot cause WIN says so:
-  # reboot pending because of some configuration action we performed
-  def reboot_pending?
-    # this key will only exit if the system need a reboot to update some file currently in use
-    # see http://technet.microsoft.com/en-us/library/cc960241.aspx
-    Registry.value_exists?('HKLM\SYSTEM\CurrentControlSet\Control\Session Manager', 'PendingFileRenameOperations') ||
-    # 1 for any value means reboot pending
-    # "9306cdfc-c4a1-4a22-9996-848cb67eddc3"=1
-    (Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired') &&
-      Registry.get_values('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired').select{|v| v[2] == 1 }.any?) ||
-    # this key will only exit if the system is pending a reboot
-    Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending') ||
-    # 1, 2 or 3 for 'Flags' value means reboot pending
-    (Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Updates\UpdateExeVolatile') &&
-      [1, 2, 3].include?(Registry.get_value('HKLM\SOFTWARE\Microsoft\Updates\UpdateExeVolatile', 'Flags')))
+  if Chef::VERSION > '11.12'
+    include Chef::DSL::RebootPending
+  else
+    # reboot cause WIN says so:
+    # reboot pending because of some configuration action we performed
+    def reboot_pending?
+      # this key will only exit if the system need a reboot to update some file currently in use
+      # see http://technet.microsoft.com/en-us/library/cc960241.aspx
+      Registry.value_exists?('HKLM\SYSTEM\CurrentControlSet\Control\Session Manager', 'PendingFileRenameOperations') ||
+      # 1 for any value means reboot pending
+      # "9306cdfc-c4a1-4a22-9996-848cb67eddc3"=1
+      (Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired') &&
+        Registry.get_values('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired').select{|v| v[2] == 1 }.any?) ||
+      # this key will only exit if the system is pending a reboot
+      ::Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending') ||
+      # 1, 2 or 3 for 'Flags' value means reboot pending
+      (Registry.key_exists?('HKLM\SOFTWARE\Microsoft\Updates\UpdateExeVolatile') &&
+        [1, 2, 3].include?(Registry.get_value('HKLM\SOFTWARE\Microsoft\Updates\UpdateExeVolatile', 'Flags')))
+    end
   end
 
   def timeout

commit 97062dc9553030d5dc10265f23255fc0cab39d98

fixes version comparison bug
Commit 7471d87e6ca4e79eeffa67cb540669d912071852 introduced a change to use Chef DSL
if the chef-client version is greater than 11.12. However, since it compares strings
the results are unexpected (for example "11.6.2" is greater than "11.12").

This change compares Gem::Version objects instead of strings.

(Thanks Jon Lenzer :D )

files/default/handlers/windows_reboot_handler.rb
@@ -52,7 +52,7 @@ class WindowsRebootHandler < Chef::Handler
     node.run_state[:reboot_requested] == true
   end
 
-  if Chef::VERSION > '11.12'
+  if Gem::Version.new(Chef::VERSION) > Gem::Version.new('11.12')
     include Chef::DSL::RebootPending
   else
     # reboot cause WIN says so: