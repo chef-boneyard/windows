d3962d217f7059de4a15be9a618ead2473635950
joemillerr <joe.miller@docusign.com>
2016-05-12 14:12:06 -0700

    Adding test for font source

--- test/cookbooks/minimal/recipes/font.rb
+++ test/cookbooks/minimal/recipes/font.rb
@@ -1,1 +1,9 @@
 windows_font 'CodeNewRoman.otf'
+
+cookbook_file 'C:/Asimov.otf' do
+  source 'Asimov.otf'
+end
+
+windows_font 'Asimov.otf' do
+  source 'C:/Asimov.otf'
+end
--- test/integration/font/pester/minimal.font.Tests.ps1
+++ test/integration/font/pester/minimal.font.Tests.ps1
@@ -6,5 +6,8 @@ describe 'minimal::font' {
     it "installs CodeNewRoman"  {
       "c:/windows/fonts/CodeNewRoman.otf" | Should Exist
     }
+    it "installs Asimov"  {
+      "c:/windows/fonts/Asimov.otf" | Should Exist
+    }
   }
 }
