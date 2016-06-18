d9db6f85915357e5623caedfe5e1c61c9543da17
Stephen Hoekstra <shoekstra@schubergphilis.com>
2016-06-19 00:35:38 +0200

    Fix broken windows_package test

--- test/cookbooks/minimal/recipes/package.rb
+++ test/cookbooks/minimal/recipes/package.rb
@@ -1,4 +1,4 @@
 windows_package 'Mercurial 3.6.1 (64-bit)' do
-  source 'http://mercurial.selenic.com/release/windows/Mercurial-3.6.1-x64.exe'
+  source 'https://www.mercurial-scm.org/release/windows/Mercurial-3.6.1-x64.exe'
   action :install
 end
