e346fb292ebe28cd6cbadcd0ef5f7121dd311ca5
Tim Smith <tsmith@chef.io>
2016-08-16 14:01:04 -0700

    Remove Chef 11 compat code from the chef_gem installs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,7 +23,7 @@
   chef_gem win_gem do
     options '--platform=mswin32'
     action :install
-    compile_time false if respond_to?(:compile_time)
+    compile_time false
   end
 end
 
@@ -31,6 +31,6 @@ end
 %w( windows-api windows-pr win32-dir win32-event win32-mutex ).each do |win_gem|
   chef_gem win_gem do
     action :install
-    compile_time false if respond_to?(:compile_time)
+    compile_time false
   end
 end
