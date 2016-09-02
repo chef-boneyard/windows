446a76d603efc42d17351c1fea81bd67f98eda0c
Tim Smith <tsmith@chef.io>
2016-09-02 15:47:34 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/task.rb
+++ resources/task.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: windows
 # Resource:: task
 #
-# Copyright:: 2012-2015, Chef Software, Inc.
+# Copyright:: 2012-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
