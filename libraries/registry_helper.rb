49a25b44360147ab6578eff03d3d80b8204ac89b
Richard Lavey <richard.lavey@calastone.com>
2016-09-26 10:58:04 +0100

    Check PROCESSOR_ARCHITECTURE for x64
    PROCESSOR_ARCHITEW6432 is null on 64-bit processes.

--- libraries/registry_helper.rb
+++ libraries/registry_helper.rb
@@ -29,7 +29,8 @@ end
 
 module Windows
   module RegistryHelper
-    @@native_registry_constant = ENV['PROCESSOR_ARCHITEW6432'] == 'AMD64' ? 0x0100 : 0x0200
+    @@native_registry_constant = ENV['PROCESSOR_ARCHITECTURE'] == 'AMD64' ||
+      ENV['PROCESSOR_ARCHITEW6432'] == 'AMD64' ? 0x0100 : 0x0200
 
     def get_hive_name(path)
       Chef::Log.debug('Resolving registry shortcuts to full names')
