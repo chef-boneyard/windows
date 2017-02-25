7a74266457545bffb268d00647f67e2e661671a4
Tim Smith <tsmith@chef.io>
2017-02-25 10:09:43 -0800

    Test pagefile create / delete
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/pagefile.rb
+++ test/cookbooks/test/recipes/pagefile.rb
@@ -1,4 +1,10 @@
-windows_pagefile 'C:\pagefile.sys' do
+windows_pagefile 'create the pagefile' do
+  name 'C:\pagefile.sys'
   initial_size 100
   maximum_size 200
+end
+
+windows_pagefile 'delete the pagefile' do
+  name 'C:\pagefile.sys'
+  action :delete
 end
