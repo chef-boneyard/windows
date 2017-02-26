b6e7bb791b7b6b14c082166508e9f1576182bbf0
Tim Smith <tsmith@chef.io>
2017-02-26 14:44:10 -0800

    Remove the default pester test
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/tasks/pester/minimal.default.Tests.ps1
+++
@@ -1,9 +0,0 @@
-$global:progressPreference = 'SilentlyContinue'
-
-describe 'File' {
-  context 'c:/chef' {
-    it 'is a directory' {
-      (get-item 'c:/chef').PSIsContainer | should be $true
-    }
-  }
-}
