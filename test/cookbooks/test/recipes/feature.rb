7e3e770d86d66609f43a32f71185f8f7ef752647
Steven Murawski <steven.murawski@gmail.com>
2017-03-09 11:32:38 -0600

    change the feature test recipe to deal with the initial appveyor machine configuration.
    Signed-off-by: Steven Murawski <steven.murawski@gmail.com>

--- test/cookbooks/test/recipes/feature.rb
+++ test/cookbooks/test/recipes/feature.rb
@@ -7,6 +7,15 @@ windows_feature 'TFTP-Client' do
   install_method :windows_feature_powershell
 end
 
+# This is for appveyor, which already seems to have FTP installed
+# which causes a short circuit of the "all" behavior and breaks the test.
+# TODO: Make :windows_feature_powershell look at all the sub-features and validate
+# that they are installed when "all is specified"
+windows_feature 'Web-Ftp-Server' do
+  action :remove
+  install_method :windows_feature_powershell
+end
+
 windows_feature 'Web-Ftp-Server' do
   action :install
   all    true
