752c23c4b71e198d9594253658bc761a91008d4f
Steven Murawski <steven.murawski@gmail.com>
2017-03-14 11:42:29 -0500

    move the `windows_share` LWRP to a custom resource and work around the segfaults on 64 bit ruby
    Signed-off-by: Steven Murawski <steven.murawski@gmail.com>

--- test/integration/share/pester/minimal.share.Tests.ps1
+++ test/integration/share/pester/minimal.share.Tests.ps1
@@ -1,57 +1,57 @@
 $global:progressPreference = 'SilentlyContinue'
 
 describe 'minimal::share' {
-  context 'windows_share' {
-    it "creates share read_only"  {
-      "\\localhost\read_only" | Should Exist
-    }
+    context 'windows_share' {
+        it "creates share read_only"  {
+            "\\localhost\read_only" | Should Exist
+        }
     
-    it "sets description"  {
-      $s = net share | Out-String
-      $s | Should Match "read_only\s*c:\\test_share\s*a test share"
-    }
+        it "sets description"  {
+            $s = Get-WmiObject win32_share -filter 'Name like "read_only"'
+            $s.description | Should Match "a test share"
+        }
     
-    it "permisisons read_only to prevent write"  {
-      $f = New-Item "\\localhost\read_only\bang" -Type File
-      $f | Should BeNullOrEmpty
-    }
+        it "permissions read_only to prevent write"  {
+            $f = New-Item "\\localhost\read_only\bang" -Type File -ErrorAction SilentlyContinue
+            $f | Should BeNullOrEmpty
+        }
     
-    it "creates share change"  {
-      "\\localhost\change" | Should Exist
-    }
+        it "creates share change"  {
+            "\\localhost\change" | Should Exist
+        }
     
-    $fileName = "\\localhost\change\change_file"
-    it "permisisons change to allow create"  {
-      $f = New-Item $fileName -Type File 
-      $f.Name | Should Be "change_file"
-    }
+        $fileName = "\\localhost\change\change_file"
+        it "permissions change to allow create"  {
+            $f = New-Item $fileName -Type File 
+            $f.Name | Should Be "change_file"
+        }
     
-    it "permisisons change to allow delete"  {
-      Remove-Item $fileName
-    }
+        it "permissions change to allow delete"  {
+            Remove-Item $fileName
+        }
     
-    it "creates share full"  {
-      "\\localhost\full" | Should Exist
-    }
+        it "creates share full"  {
+            "\\localhost\full" | Should Exist
+        }
     
-    $fileName = "\\localhost\full\change_file"
-    it "permisisons full to allow create"  {
-      $f = New-Item $fileName -Type File 
-      $f.Name | Should Be "change_file"
-    }
+        $fileName = "\\localhost\full\change_file"
+        it "permissions full to allow create"  {
+            $f = New-Item $fileName -Type File 
+            $f.Name | Should Be "change_file"
+        }
     
-    it "permisisons full to allow delete"  {
-      Remove-Item $fileName
-    }
+        it "permissions full to allow delete"  {
+            Remove-Item $fileName
+        }
     
-    it "removes share no_share"  {
-      "\\localhost\no_share" | Should Not Exist
-    }
+        it "removes share no_share"  {
+            "\\localhost\no_share" | Should Not Exist
+        }
     
-    it "can change the directory associated with a share"  {
-      $s = net share | Out-String
-      $s | Should Match "changed_dir\s*c:\\test_share"
-    }
+        it "can change the directory associated with a share"  {
+            $s = Get-WmiObject win32_share -filter 'Name like "changed_dir"'
+            $s.path | Should Match "c:/test_share"
+        }
     
-  }
+    }
 }
