dd4478336f9c963b7a39f424a4945e51cd783ee8
Tim Smith <tsmith@chef.io>
2018-02-23 12:42:13 -0800

    mixlib shellout is part of resource so we don't need to include it
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/pagefile.rb
+++ resources/pagefile.rb
@@ -3,8 +3,8 @@
 # Cookbook:: windows
 # Resource:: pagefile
 #
-# Copyright:: 2012-2017, Nordstrom, Inc.
-# Copyright:: 2017, Chef Software, Inc.
+# Copyright:: 2012-2018, Nordstrom, Inc.
+# Copyright:: 2017-2018, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
