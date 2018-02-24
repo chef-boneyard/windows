334a6747d5af768efb66c41b3a9de9211ecb0cf1
Tim Smith <tsmith@chef.io>
2018-02-23 16:33:58 -0800

    Convert autorun from pester to InSpec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/autorun.rb
+++ test/cookbooks/test/recipes/autorun.rb
@@ -1,6 +1,6 @@
 windows_auto_run 'add notepad' do
   program_name 'notepad'
-  path 'C:/windows/system32/notepad.exe'
+  path 'C:\windows\system32\notepad.exe'
   action :create
 end
 
@@ -12,7 +12,7 @@ end
 
 windows_auto_run 'add wordpad' do
   program_name 'wordpad'
-  program 'C:\Windows\System32\write.exe' # the legacy name for the path property
+  program 'C:/Windows/System32/write.exe' # the legacy name for the path property
   root :user
   action :create
 end
