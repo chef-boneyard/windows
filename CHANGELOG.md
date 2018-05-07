fb59bdf8491701437d6e7286bbadafbab10501e5
Tim Smith <tsmith@chef.io>
2018-05-07 09:24:56 -0700

    Release 4.2.3 Signed-off-by: Tim Smith <tsmith@chef.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the windows cookbook.
 
+## 4.2.3 (2018-05-07)
+
+- Include the helper in the action class to prevent failures with the zipfile resource
+
 ## 4.2.2 (2018-04-24)
 
 - Properly fail in windows_share on Windows 2008 R2 since we lack the cmdlets to manipulates shares on those systems.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Provides a set of useful Windows-specific primitives.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.2.2'
+version          '4.2.3'
 supports         'windows'
 source_url       'https://github.com/chef-cookbooks/windows'
 issues_url       'https://github.com/chef-cookbooks/windows/issues'
