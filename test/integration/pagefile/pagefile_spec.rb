b5c997a68fe6b1576597cd36122dc167d9db69d7
Tim Smith <tsmith@chef.io>
2018-05-29 11:30:44 -0700

    Convert path test to inspec and make it work on Chef 13+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/pagefile/pagefile_spec.rb
+++ test/integration/pagefile/pagefile_spec.rb
@@ -4,5 +4,5 @@ end
 
 describe command('wmic pagefileset') do
   its('exit_status') { should eq 0 } # if it was system managed it would be 1
-  its('stderr') { should_not match (/No Instance\(s\) Available/) } # not system managed
+  its('stderr') { should_not match /No Instance\(s\) Available/ } # not system managed
 end
