15e45304edd9a7ffb08824d34381760a26fc0996
Tim Smith <tsmith@chef.io>
2018-09-06 08:47:09 -0700

    Remove deprecated resources that are in Chef 14+
    These resources now ship in Chef 14 or later. There's no need to ship
    them in this recipe as well.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/autorun/autorun_spec.rb
+++
@@ -1,7 +0,0 @@
-describe registry_key('HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run') do
-  its('wordpad') { should eq '"C:\\Windows\\System32\\write.exe"' }
-end
-
-describe registry_key('HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\notepad') do
-  it { should_not exist }
-end
