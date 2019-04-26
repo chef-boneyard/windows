096a96f8af31efae8e8a6097af0c7e56120e95b2
Tim Smith <tsmith@chef.io>
2019-04-25 21:31:38 -0700

    Remove deprecated resources that are in Chef 14+ (#574)
    Remove deprecated resources that are in Chef 14+

--- test/integration/pagefile/pagefile_spec.rb
+++
@@ -1,8 +0,0 @@
-describe file('c:/pagefile.sys') do
-  it { should exist }
-end
-
-describe command('wmic pagefileset') do
-  its('exit_status') { should eq 0 } # if it was system managed it would be 1
-  its('stderr') { should_not match /No Instance\(s\) Available/ } # not system managed
-end
