65530ed6443a22e2c530a8f8bd4b7c46a46b9c53
Xorima <4923914+Xorima@users.noreply.github.com>
2019-09-30 18:30:16 +0100

    Fix build failures and cookstyle cleanup (#618)
    * Accept Chef License
    
    Accept the chef license
    
    * Cookstyle latest
    
    Fix the latest cookstyle issues
    Make builds run against chef 14
    
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>
    
    * Requested changes
    
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Provides a set of useful Windows-specific primitives.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '6.0.0'
 supports         'windows'
 source_url       'https://github.com/chef-cookbooks/windows'
